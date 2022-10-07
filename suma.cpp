c8da9d4725d303a9bb826803aefc881e1bc890f5
viviana <viviana.preciado.ortiz@utelvt.edu.ec>
2022-10-07 08:51:54 -0500

    version 1

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+int main ()
+{
+	float A,B,C;
+	cout<<"Ingrese dos numeros A B: ";
+	cin>>A>>B;
+	C=A+B;
+	float x,y,z;
+	cout<<"Ingrese dos numeros separados de espacio x y:  ";
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resultado es: ";
+	cout<<C;
+	cout<<z<<endl;
+	return 0;
+}
