cf16f943afa2976a9ac83b8340492e8eba119ad3
viviana <viviana.preciado.ortiz@utelvt.edu.ec>
2022-10-07 09:20:14 -0500

    version1

---
+++ compara.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float VAPO_a,VAPO_b;
+	cout<<"ingrese VAPO_a VAPO_b:"; cin>>VAPO_a>>VAPO_b;
+	if(VAPO_a>VAPO_b)
+	{
+	  cout<<VAPO_a<<" es mayor VAPO_a "<<VAPO_b<<endl;
+
+
+	}else{
+
+	  cout<<VAPO_b<<" es mayor VAPO_a "<<VAPO_a<<endl;
+
+
+	}
+
+	return 0;
+
+
+
+
+
+
+}
